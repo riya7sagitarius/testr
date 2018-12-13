73738ceb7df7c03d6d4b0f5904364a476ea3dfeb
john eipe <john77eipe@gmail.com>
2018-12-13 17:11:13 +0530

    StoryA commit

--- projectOne/src/main/java/com/test/App.java
+++ projectOne/src/main/java/com/test/App.java
@@ -2,12 +2,11 @@ package com.test;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+        System.out.println("story a implemented");
     }
 }
