5761e499cb593c570a77990457eca2cfe05c8762
john eipe <john77eipe@gmail.com>
2018-12-13 20:37:16 +0530

    StoryA commit : code updated correct

--- projectOne/src/main/java/com/test/App.java
+++ projectOne/src/main/java/com/test/App.java
@@ -8,6 +8,7 @@ public class App {
 
         System.out.println("Hello World!");
         System.out.println("story a implemented");
-        System.out.println("story a implemented wrong");
+
+        System.out.println("story a implemented correct");
     }
 }
